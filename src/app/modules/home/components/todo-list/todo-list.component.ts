0817a469d1f83dafdfccd4d0d50286b404cbae9e
davidGBbr <davidgomesbrigido@gmail.com>
2023-08-30 17:52:24 -0300

    Hands-on 8

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,6 +11,16 @@ export class TodoListComponent implements OnInit {
     { task: 'Minha nova Task', checked: true },
     { task: 'Minha nova Task 2', checked: false },
   ];
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja realmente Deletar tudo?');
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
   constructor() {}
   ngOnInit(): void {}
 }
